316326c1c69ee1dbbb82e3d45318eaa6b2746689
guimartinsa <guilhermemaretinsyoutub13@gmail.com>
2023-02-05 11:59:38 -0300

    add initial menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function clickmenu() {
+    var menu = document.getElementById("menu-b");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
